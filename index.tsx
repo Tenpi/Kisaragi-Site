f2f64850d1ced8b5bb7f6c4e16bdd307b2a66d2f
Tenpi <37512637+Tenpi@users.noreply.github.com>
2020-08-05 17:24:04 -0400

    small

--- index.tsx
+++ index.tsx
@@ -4,6 +4,6 @@ import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 import images from "./structures/Images"
 
-ReactDOM.hydrate(<App/>, document.getElementById("root"))
+ReactDOM.render(<App/>, document.getElementById("root"))
 // images.animateFavicon()
 serviceWorker.unregister()
